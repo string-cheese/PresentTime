8011efa2ccb6d43d97bac5f697eb69e0a352e200
David Taylor <David.Taylor@wavetronix.com>
2016-03-20 23:32:09 -0600

    changed pager to fragmentpager

---
+++ app/src/main/java/edu/byu/stringcheese/presenttime/slidingtab/DashboardSectionFragment.java
@@ -0,0 +1,22 @@
+package edu.byu.stringcheese.presenttime.slidingtab;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import edu.byu.stringcheese.presenttime.R;
+
+/**
+ * Created by dtaylor on 3/20/2016.
+ */
+public class DashboardSectionFragment extends android.support.v4.app.Fragment {
+    @Override
+      public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                               Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.dashboard_section_fragment, container, false);
+        /*TextView dummyTextView = (TextView) rootView.findViewById(R.id.section_label);
+        dummyTextView.setText(Integer.toString(getArguments().getInt(ARG_SECTION_NUMBER)));*/
+        return rootView;
+    }
+}
--- app/src/main/java/edu/byu/stringcheese/presenttime/slidingtab/SlidingTabsBasicFragment.java
+++ app/src/main/java/edu/byu/stringcheese/presenttime/slidingtab/SlidingTabsBasicFragment.java
@@ -2,12 +2,14 @@ package edu.byu.stringcheese.presenttime.slidingtab;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v4.view.PagerAdapter;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import java.util.Locale;
 
 import edu.byu.stringcheese.presenttime.R;
 
@@ -49,7 +51,7 @@ public class SlidingTabsBasicFragment extends Fragment {
     public void onViewCreated(View view, Bundle savedInstanceState) {
         // Get the ViewPager and set it's PagerAdapter so that it can display items
         mViewPager = (ViewPager) view.findViewById(R.id.viewpager);
-        mViewPager.setAdapter(new SamplePagerAdapter());
+        mViewPager.setAdapter(new SamplePagerAdapter(getFragmentManager()));
 
         // Give the SlidingTabLayout the ViewPager, this must be done AFTER the ViewPager has had
         // it's PagerAdapter set.
@@ -63,43 +65,118 @@ public class SlidingTabsBasicFragment extends Fragment {
      * this class is the {@link #getPageTitle(int)} method which controls what is displayed in the
      * {@link SlidingTabLayout}.
      */
-    class SamplePagerAdapter extends PagerAdapter {
+    class SamplePagerAdapter extends FragmentPagerAdapter {
 
-        private static final int NUM_PAGES = 3;
+        private static final int NUM_PAGES = 4;
+        public SamplePagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
 
+        // BEGIN_INCLUDE (fragment_pager_adapter_getitem)
         /**
-         * @return the number of pages to display
+         * Get fragment corresponding to a specific position. This will be used to populate the
+         * contents of the {@link ViewPager}.
+         *
+         * @param position Position to fetch fragment for.
+         * @return Fragment for specified position.
+         */
+        @Override
+        public Fragment getItem(int position) {
+            // getItem is called to instantiate the fragment for the given page.
+
+            Fragment fragment;
+            switch (position) {
+                case 0:
+                    fragment = new DashboardSectionFragment();
+                    break;
+                case 1:
+                    fragment = new EventsSectionFragment();
+                    break;
+                case 2:
+                    fragment = new FriendsSectionFragment();
+                    break;
+                case 3:
+                    fragment = new ProfileSectionFragment();
+                    break;
+                default:
+                    fragment = new DashboardSectionFragment();
+                    break;
+            }
+            /*Bundle args = new Bundle();
+            args.putInt(DummySectionFragment.ARG_SECTION_NUMBER, position + 1);
+            fragment.setArguments(args);*/
+            return fragment;
+        }
+        // END_INCLUDE (fragment_pager_adapter_getitem)
+
+        // BEGIN_INCLUDE (fragment_pager_adapter_getcount)
+        /**
+         * Get number of pages the {@link ViewPager} should render.
+         *
+         * @return Number of fragments to be rendered as pages.
          */
         @Override
         public int getCount() {
             return NUM_PAGES;
         }
+        // END_INCLUDE (fragment_pager_adapter_getcount)
+
+        // BEGIN_INCLUDE (fragment_pager_adapter_getpagetitle)
+        /**
+         * Get title for each of the pages. This will be displayed on each of the tabs.
+         *
+         * @param position Page to fetch title for.
+         * @return Title for specified page.
+         */
+        @Override
+        public CharSequence getPageTitle(int position) {
+            Locale l = Locale.getDefault();
+            switch (position) {
+                case 0:
+                    return getString(R.string.title_section1).toUpperCase(l);
+                case 1:
+                    return getString(R.string.title_section2).toUpperCase(l);
+                case 2:
+                    return getString(R.string.title_section3).toUpperCase(l);
+                case 3:
+                    return getString(R.string.title_section4).toUpperCase(l);
+            }
+            return null;
+        }
 
         /**
+         * @return the number of pages to display
+        @Override
+        public int getCount() {
+            return NUM_PAGES;
+        }
+
+        *//**
          * @return true if the value returned from {@link #instantiateItem(ViewGroup, int)} is the
          * same object as the {@link View} added to the {@link ViewPager}.
-         */
+         *//*
         @Override
         public boolean isViewFromObject(View view, Object o) {
             return o == view;
         }
 
-        /**
+        *//**
          * Return the title of the item at {@code position}. This is important as what this method
          * returns is what is displayed in the {@link SlidingTabLayout}.
          * <p>
          * Here we construct one using the position value, but for real application the title should
          * refer to the item's contents.
-         */
+         *//*
         @Override
         public CharSequence getPageTitle(int position) {
             return "Item " + (position + 1);
         }
 
-        /**
+
+        *//**
          * Instantiate the {@link View} which should be displayed at {@code position}. Here we
          * inflate a layout from the apps resources and then change the text view to signify the position.
-         */
+         *//*
         @Override
         public Object instantiateItem(ViewGroup container, int position) {
             int id = R.layout.pager_item;
@@ -131,15 +208,15 @@ public class SlidingTabsBasicFragment extends Fragment {
             return view;
         }
 
-        /**
+        *//**
          * Destroy the item from the {@link ViewPager}. In our case this is simply removing the
          * {@link View}.
-         */
+         *//*
         @Override
         public void destroyItem(ViewGroup container, int position, Object object) {
             container.removeView((View) object);
             Log.i(LOG_TAG, "destroyItem() [position: " + position + "]");
-        }
+        }*/
 
     }
 }
